196feb6368706e8ec1ace41b4657a98481fddcf3
studywzy <1365997350@qq.com>
2020-02-24 00:22:43 +0800

    类锁和死锁

---
+++ Thread/src/com/Thread/ThreadTest12.java
@@ -0,0 +1,80 @@
+package com.Thread;
+
+/**
+ * @author Wzy
+ * @date 2020-02-23 - 23:22
+ *
+ * 类锁，类只有一个，所以锁是类级别的，只有一个
+ */
+public class ThreadTest12 {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Processor11());
+        Thread t2 = new Thread(new Processor11());
+
+        t1.setName("t1");
+        t2.setName("t2");
+
+        t1.start();
+
+        //保证m1方法会执行
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        t2.start();
+
+    }
+}
+
+class Processor11 implements Runnable{
+
+    public void run(){
+
+        if("t1".equals(Thread.currentThread().getName())){
+
+            MyClass.m1();
+        }
+        if("t2".equals(Thread.currentThread().getName())){
+
+            MyClass.m2();
+        }
+    }
+}
+class MyClass{
+
+    //synchronized添加到静态方法上，线程执行此方法的时候会找类锁
+    public synchronized static void m1(){
+
+        try {
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("m1.....");
+    }
+
+    //不会等m1结束，因为该方法没有被synchronized修饰
+    /*
+    public static void m2(){
+
+        System.out.println("m2.....");
+    }
+    */
+
+    //m2等m1运行结束后才会执行，因为类锁只有一个并且被m1拿走了
+    public synchronized static void m2(){
+
+        System.out.println("m2.....");
+    }
+}
+
+/*
+第一次运行结果（修饰掉的部分）：m2.....
+                          m1.....
+
+第二次运行结果（没修饰的部分）：
+ */
---
+++ Thread/src/com/Thread/ThreadTest13.java
@@ -0,0 +1,77 @@
+package com.Thread;
+
+/**
+ * @author Wzy
+ * @date 2020-02-23 - 23:47
+ *
+ * 死锁
+ */
+public class ThreadTest13 {
+
+    public static void main(String[] args) {
+
+        Object o1 = new Object();
+        Object o2 = new Object();
+
+        Thread t1 = new Thread(new T1(o1,o2));
+        Thread t2 = new Thread(new T2(o1,o2));
+
+        t1.start();
+
+        t2.start();
+    }
+}
+
+class T1 implements Runnable{
+
+    Object o1;
+    Object o2;
+
+    public T1(Object o1, Object o2) {
+        this.o1 = o1;
+        this.o2 = o2;
+    }
+
+    public void run(){
+
+        synchronized (o1){
+
+            try {
+                Thread.sleep(1000);
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            synchronized (o2){
+
+            }
+        }
+    }
+}
+
+class T2 implements Runnable{
+
+    Object o1;
+    Object o2;
+
+    public T2(Object o1, Object o2) {
+        this.o1 = o1;
+        this.o2 = o2;
+    }
+
+    public void run(){
+        synchronized (o2){
+
+            try {
+                Thread.sleep(1000);
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+            synchronized (o1){
+
+            }
+        }
+    }
+}
